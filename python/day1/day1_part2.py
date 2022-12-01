3b94112f38f9e18f34f93619dadd622b916ff660
Kris Austin <kris.austin@draft2digital.com>
2022-12-01 07:57:46 -0600

    Removed unnecessary variable from Python Day 1

--- python/day1/day1_part2.py
+++ python/day1/day1_part2.py
@@ -1,13 +1,11 @@
 # Part 2
 with open('day1_input.txt') as f:
     elves = []
-    elf_number = 0
     elf_total = 0
 
     for l in f.readlines():
         if not l.strip():
             elves.append(elf_total)
-            elf_number += 1
             elf_total = 0
         else:
             elf_total += int(l)
