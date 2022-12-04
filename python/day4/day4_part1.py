f5cc1e901c2b2e18388eae65b0921a0382555454
Kris Austin <kris.austin@draft2digital.com>
2022-12-04 13:02:09 -0600

    Python Day 4

---
+++ python/day4/day4_part1.py
@@ -0,0 +1,14 @@
+pairs = list(map(lambda x: x.strip().split(","), open("day4_input.txt")))
+
+overlaps = 0
+for e1, e2 in pairs:
+    e1_start, e1_end = map(int, e1.split("-"))
+    e1_assignments = set(range(e1_start, e1_end + 1))
+
+    e2_start, e2_end = map(int, e2.split("-"))
+    e2_assignments = set(range(e2_start, e2_end + 1))
+
+    if e1_assignments <= e2_assignments or e2_assignments <= e1_assignments:
+        overlaps += 1
+
+print(overlaps)
